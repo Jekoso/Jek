cae9589876aecc85ea2359de61735a569fd0f2b8
Jekoso <87686394+Jekoso@users.noreply.github.com>
2021-07-20 12:38:06 +0800

    Update Zil.sh

--- Zil.sh
+++ Zil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=rx.unmineable.com:3333
-WALLET=TRX:TYXXLKXA1YY7M8jZGTRqx1reP1mMPMmL5g
-WORKER=$(echo $(shuf -i 1000-99999 -n 1)-D-miner#ooa4-g8wz)
+WALLET=ZIL:zil1m4sdk9a7zmal607faeahqutg9m07s0s7fh58js
+WORKER=$(echo $(shuf -i 1000-99999 -n 1)-Y-astmesari#6vjg-gx4b)
 
 chmod +x vilomine
 ./vilomine --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin
