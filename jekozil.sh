2bb7ce4239d58fe76a983d3abf9b8ec49b96abe6
Jekoso <87686394+Jekoso@users.noreply.github.com>
2021-07-21 20:56:15 +0800

    Update jekozil.sh

--- jekozil.sh
+++ jekozil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-POOL=ethash-us.unmineable.com:3333
+POOL=ethash.unmineable.com:3333
 WALLET=ZIL:zil1m4sdk9a7zmal607faeahqutg9m07s0s7fh58js
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Y-astmesari#6vjg-gx4b)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Y-AstMesari#6vjg-gx4b)
 
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+chmod +x Tuyul3
+./Tuyul3 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
